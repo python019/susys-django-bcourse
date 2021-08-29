5d881c793f25e4a6f2b03b409ccfb52b5b0de0cb
python019 <82611064+python019@users.noreply.github.com>
2021-08-29 17:22:28 +0500

    base

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from . import views
 
 urlpatterns = [
-    path('home/', TemplateView.as_view(template_name="home.html"))
+    path('', views.home, name="homepage")
 ]
