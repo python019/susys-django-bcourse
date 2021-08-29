faf6c40140aa1a9275cc83e61c58a8e0da7b9adb
python019 <82611064+python019@users.noreply.github.com>
2021-08-29 18:35:16 +0500

    base

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
