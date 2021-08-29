e677926011351c37d371c1544f137677d019b6f5
python019 <82611064+python019@users.noreply.github.com>
2021-08-29 15:20:06 +0500

    base

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from django.views.generic import TemplateView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', TemplateView.as_view(template_name="home.html"))
+    path('', include('backend.urls'))
 ]
