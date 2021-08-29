d14d0c9a3f74c9cd36d658080cadb9ff5f6c4e63
python019 <82611064+python019@users.noreply.github.com>
2021-08-30 00:41:21 +0500

    base

--- backend/models.py
+++ backend/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
+    slug = models.SlugField(max_length=50)
 
     def __str__(self):
-        return self.title
+        return self.slug
