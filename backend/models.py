082d45600a0e168f86c976f9c8f29f4485ab3179
python019 <82611064+python019@users.noreply.github.com>
2021-09-03 18:45:51 +0500

    base

--- backend/models.py
+++ backend/models.py
@@ -4,6 +4,7 @@ class Post(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     slug = models.SlugField(max_length=50)
+    image = models.ImageField(upload_to="images/", default="images/defult.png")
 
     def __str__(self):
         return self.slug
